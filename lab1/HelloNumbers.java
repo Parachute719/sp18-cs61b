2d21498f858a083b4e1e338810b4d02fb49d7ccb
张轩 <zhangxuan@zhangxuandeMacBook-Pro.local>
2020-06-24 11:37:12 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,15 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        int sum =0;
+        while (x < 10) {
+        	sum = sum + x;
+            System.out.println(sum);
+
+            x = x + 1;
+        }
+        System.out.println(5 + "horse");
+    }
+}// this is a java comment */
+// this is another comment /*
+/** this is the third comment */
